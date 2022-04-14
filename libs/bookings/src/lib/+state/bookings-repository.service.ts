fb734065ca0c6799ed685b71ae35aa19bda63608
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-04-14 12:15:43 +0200

    update bookings with viewmodel

--- libs/bookings/src/lib/+state/bookings-repository.service.ts
+++ libs/bookings/src/lib/+state/bookings-repository.service.ts
@@ -1,34 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CustomersApi } from '@eternal/customers/api';
-import { combineLatest, map, Observable } from 'rxjs';
 import { Booking, bookingsFeature } from './bookings.reducer';
-import { assertDefined } from '@eternal/shared/util';
-import { filterDefined } from '@eternal/shared/ngrx-utils';
-
-interface BookingData {
-  bookings: Booking[];
-  customerName: string;
-  loaded: boolean;
-}
+import * as bookingsActions from './bookings.actions';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class BookingsRepository {
-  readonly bookingData$: Observable<BookingData> = combineLatest({
-    customer: this.customersApi.selectedCustomer$,
-    bookings: this.store.select(bookingsFeature.selectBookings),
-    loaded: this.store.select(bookingsFeature.selectLoaded),
-  }).pipe(
-    filterDefined,
-    map(({ customer, bookings, loaded }) => {
-      assertDefined(customer);
-      return {
-        customerName: customer.name + ', ' + customer.firstname,
-        bookings,
-        loaded,
-      };
-    })
+  readonly bookinkgs$: Observable<Booking[]> = this.store.select(
+    bookingsFeature.selectBookings
   );
-
+  readonly loaded$: Observable<boolean> = this.store.select(
+    bookingsFeature.selectLoaded
+  );
   constructor(private store: Store, private customersApi: CustomersApi) {}
+
+  load(): void {
+    this.store.dispatch(bookingsActions.load());
+  }
 }
