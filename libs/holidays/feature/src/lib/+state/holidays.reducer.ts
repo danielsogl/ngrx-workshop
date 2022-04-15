bf60d1a687efb6154ee5b444b9300455063e5428
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-04-15 11:19:22 +0200

    add data to reducer

--- libs/holidays/feature/src/lib/+state/holidays.reducer.ts
+++ libs/holidays/feature/src/lib/+state/holidays.reducer.ts
@@ -1,12 +1,14 @@
 import { Holiday } from '@eternal/holidays/model';
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { found } from './holidays.actions';
+import { LoadStatus } from '@eternal/shared/ngrx-utils';
 
 export interface HolidaysState {
   holidays: Holiday[];
+  loadStatus: LoadStatus;
 }
 
-const initialState: HolidaysState = { holidays: [] };
+const initialState: HolidaysState = { holidays: [], loadStatus: 'not loaded' };
 
 export const holidaysFeature = createFeature({
   name: 'holiday',
