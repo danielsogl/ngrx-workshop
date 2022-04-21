4d0025b8614b4909309f32365311b8e6b882382c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-04-21 14:47:16 +0200

    add tab synchronization and rehydration

---
+++ libs/core/local-storage-state/src/lib/local-storage.effects.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { createEffect } from '@ngrx/effects';
+import { fromEvent, pluck } from 'rxjs';
+import { filterDefined } from '@eternal/shared/ngrx-utils';
+import { map } from 'rxjs/operators';
+import { syncLocalStorage } from './sync-local-storage.action';
+
+@Injectable()
+export class LocalStorageEffects {
+  constructor() {}
+
+  localStorageChange$ = createEffect(() =>
+    fromEvent<StorageEvent>(window, 'storage').pipe(
+      pluck('key'),
+      filterDefined,
+      map((key) => syncLocalStorage({ featureState: key }))
+    )
+  );
+}
